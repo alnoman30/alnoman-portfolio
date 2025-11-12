26f287f313b2cbd1bea1063b2adc3f81f3bf69a3
noman <alnomaan30@gmail.com>
2025-11-12 14:56:18 +0600

    hero section

--- src/components/hero/Hero.jsx
+++ src/components/hero/Hero.jsx
@@ -2,11 +2,14 @@ import React from "react";
 import { motion } from "framer-motion";
 import ProfileCard from "./ProfileCard";
 
+import { RiGithubLine } from "react-icons/ri";
+import { FiLinkedin } from "react-icons/fi";
+import { FaRegEnvelope } from "react-icons/fa";
+
 const Hero = () => {
   return (
     <section className="min-h-screen flex items-center py-20">
       <div className="container mx-auto flex flex-col-reverse md:flex-row items-center justify-between px-6 md:px-4">
-        
         {/* Left Section */}
         <motion.div
           className="text-left max-w-xl"
@@ -33,7 +36,7 @@ const Hero = () => {
           </h1>
 
           {/* Subheading */}
-          <p className="text-xl md:text-2xl text-gray-600 mb-6 font-medium">
+          <p className="text-xl md:text-2xl text-gray-600 dark:text-[#F2F2F2] mb-6 font-medium">
             FullStack Developer specializing in MERN stack
           </p>
 
@@ -44,7 +47,7 @@ const Hero = () => {
           </p>
 
           {/* Buttons */}
-          <div className="flex flex-wrap justify-center md:justify-start gap-4">
+          <div className="flex flex-wrap justify-start md:justify-start gap-4">
             <motion.button
               whileHover={{ scale: 1.05 }}
               whileTap={{ scale: 0.95 }}
@@ -62,10 +65,16 @@ const Hero = () => {
           </div>
 
           {/* Social icons */}
-          <div className="flex justify-center md:justify-start gap-6 mt-8 text-2xl text-gray-600">
-            <i className="fa-brands fa-github hover:text-purple-600 transition"></i>
-            <i className="fa-brands fa-linkedin hover:text-purple-600 transition"></i>
-            <i className="fa-solid fa-envelope hover:text-purple-600 transition"></i>
+          <div className="flex justify-start md:justify-start gap-6 mt-8 text-2xl text-gray-600">
+            <li className="bg-[#F3E7F9] hover:bg-violet-400 hover:text-white cursor-pointer p-3 rounded-full transition duration-300 transform hover:-translate-y-2">
+              <a href="https://github.com/alnoman30" target="_blank"><RiGithubLine /></a>
+            </li>
+            <li className="bg-[#F3E7F9] hover:bg-violet-400 hover:text-white cursor-pointer p-3 rounded-full transition duration-300 transform hover:-translate-y-2">
+              <a href="https://www.linkedin.com/in/abdullah-al-noman30/" target="_blank"><FiLinkedin /></a>
+            </li>
+            <li className="bg-[#F3E7F9] hover:bg-violet-400 hover:text-white cursor-pointer p-3 rounded-full transition duration-300 transform hover:-translate-y-2">
+              <a href="mailto:alnomaan30@gmail.com" target="_blank"><FaRegEnvelope /></a>
+            </li>
           </div>
         </motion.div>
 
@@ -78,7 +87,6 @@ const Hero = () => {
         >
           <ProfileCard />
         </motion.div>
-
       </div>
     </section>
   );
