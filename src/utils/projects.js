a85fd8f38ec5d1ccf88e1c12967ea1e351f1608c
noman <alnomaan30@gmail.com>
2025-11-17 13:10:26 +0600

    project uploads

--- src/utils/projects.js
+++ src/utils/projects.js
@@ -1,15 +1,26 @@
 // Sample data array
 import project1 from "../assets/project1.png";
+import project1 from "../assets/project2.png";
 
 
 const projects = [
   {
     id: 1,
-    category: "UI-UX DESIGN",
-    title: "Product Admin Dashboard",
+    category: "Landing Page",
+    title: "Panto - Furniture Landing page",
     description:
       "Vivamus eleifend convallis ante, non pharetra libero molestie laoreet. Donec id imperdiet lacus.",
     image: project1,
+    github: "https://github.com/example/project1",
+    preview: "https://example.com/project1",
+  },
+  {
+    id: 2,
+    category: "Restaurant",
+    title: "RainyRoof Restaurant & Cafe",
+    description:
+      "Vivamus eleifend convallis ante, non pharetra libero molestie laoreet. Donec id imperdiet lacus.",
+    image: project2,
     github: "https://github.com/example/project1",
     preview: "https://example.com/project1",
   },
